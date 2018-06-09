f331342a89350aeb57c8567f1012a03eba7fa2f5
Raymond Xie <yuhua.xie@gmail.com>
2018-06-09 11:05:08 -0700

    add HTML graphics topic

---
+++ interactive-class/graphics/canvas.js
@@ -0,0 +1,38 @@
+function drawLine(x, y, w, h) {
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    ctx.moveTo(x, y);
+    ctx.lineTo(w, h);
+    ctx.stroke();
+}
+
+
+function drawCircle() {
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    ctx.beginPath();
+    ctx.arc(195, 300, 40, 0, 2 * Math.PI);
+    ctx.stroke();
+}
+
+function drawText(text) {
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    ctx.font = "30px Arial";
+    // ctx.fillText(text, 10, 50);
+    ctx.strokeText(text, 10, 50);
+}
+
+function drawGradient() {
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+
+    // Create gradient
+    var grd = ctx.createLinearGradient(0, 0, 200, 0);
+    grd.addColorStop(0, "red");
+    grd.addColorStop(1, "white");
+
+    // Fill with gradient
+    ctx.fillStyle = grd;
+    ctx.fillRect(20, 100, 150, 80);
+}
